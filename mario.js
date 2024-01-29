ba828e76699b347cbda3926db3ce1a625674b3a8
amrbataineh <155365048+amrbataineh@users.noreply.github.com>
2024-01-30 00:52:27 +0300

    edit mario

--- mario.js
+++ mario.js
@@ -1,32 +1,118 @@
-function MoveMario(x){
-let marioBtt = document.getElementsByClassName('marioBtt')[x].value;
-let mainimge = document.getElementsByClassName('mainimge'); 
-if(marioBtt === 'Left'){
-    let mainimge  =  Left;
-   
-     }
-     else if(marioBtt === 'Move right'){
-        let mainimge=  right;
-   
-     }
-     else if( marioBtt=== 'Move to up '){
-        let mainimge =   up ; 
-   
-     }
-     else if(marioBtt === 'Move Down'){
-        let mainimge = Down ;
-   
-   
-     }
-     document.getElementsByClassName('marioBtt')[x].innerHTML
-
-     document.getElementsByClassName('mainimge')[x].innerHTML
+var mario = document.getElementById('mario');
+var marioButt = document.getElementsByClassName('marioButt');
+mario.style.margin=0
+function moveMario(index){
+   var step = 10;
+ 
+if (index == 0)
+{
+ mario.src ='./media/mario left.jpg'
+var mariomoveLeft = mario.style.marginLeft;
+console.log( parseInt(mariomoveLeft));
+//console.log( parseInt('20px'))
+mario.style.marginLeft = parseInt(mariomoveLeft) +10+ 'px';
+if((parseInt(mariomoveLeft) +10)>950){
+   alert('you can not move left')
 
 }
+else{
+   mario.style.marginLeft = parseInt(mariomoveLeft) +10+ 'px';
+
+}
+
+}
+else if (index == 1)
+{
+   mario.src ='./media/mario right.jpg'
+   var mariomoveLeft = mario.style.marginLeft;
+if( (parseInt(mariomoveLeft)-10) < 0 ){
+   alert('you can not move right')
+}
+else{
+   mario.style.marginLeft = parseInt(mariomoveLeft) -10+ 'px';
+ 
+}
+
+}
+else if (index == 2)
+{ 
+   mario.src ='./media/mario up.jpg'
+   var mariomovedown = mario.style.marginTop
+   mario.style.marginTop= parseInt(mariomovedown) -10+ 'px';
+
+if((parseInt(mariomovedown) -10)<0)
+{
+   alert('you can not move up')
+
+
+}
+else{
+   mario.style.marginTop= parseInt(mariomovedown) -10+ 'px';
+
+}
+
+
+
+}
+else if (index == 3)
+{
+   mario.src ='./media/mario down.jpg'
+   var mariomovedown = mario.style.marginTop
+if((parseInt(mariomovedown) +10)>650){
+   alert('you can not move down')
+
+}
+else{
+   mario.style.marginTop= parseInt(mariomovedown) +10+ 'px';
+
+}
+
+
+}
+
+}
+function moveAround(){
+   var randomIndex = Math.floor(Math.random() * 4);
+    moveMario(randomIndex);
+
+}
+function moveAroundRandomly() {
+   setInterval(moveRandom, 1000); 
+}
+function stopMove(){
+   clearInterval(stop);
+   alert('Movement has been stopped')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //حلي انا كان بطريقة ال switch
    // switch( sympol){
-     
-/*
+     /*
 switch( MoveMario){
    case ' Left':
        marioBtt =  Left;
@@ -46,4 +132,4 @@ switch( MoveMario){
     
     }
     document.getElementsByClassName('marioBtt')[x].innerHTML
-    */
+*/
